7d5edd82b19951725a83f58387c28030a2ad61d5
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-11-11 22:48:14 +0100

    Refactor constants to export default values for distance, consumption, and price

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,5 @@
 // constants.js
 
-// eslint-disable-next-line no-unused-vars
-const DEFAULT_DISTANCE = 1;
-
-// eslint-disable-next-line no-unused-vars
-const DEFAULT_CONSUMPTION = 8;
-
-// eslint-disable-next-line no-unused-vars
-const DEFAULT_PRICE = 5;
+export const DEFAULT_DISTANCE = 1;
+export const DEFAULT_CONSUMPTION = 8;
+export const DEFAULT_PRICE = 5;
